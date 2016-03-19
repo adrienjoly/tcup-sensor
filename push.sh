2465f2ccb038c3d227bf22424be62363681973f2
Adrien Joly <adrien@whyd.com>
2016-03-19 18:46:48 +0100

    push is working!

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 # rsync -av -e "ssh -A root@proxy ssh" ./ root@192.168.100.177/node_target
 # rsync -avz ./ root@192.168.100.177:/node_app_slot/
 # rcp -r ./ root@192.168.100.177:/node_app_slot/
-rcp -r ./ root@edison.local:/node_app_slot/
+# rcp -r ./ root@edison.local:./
+
+# if you want the script to run automatically on startup, thanks to XDK deamon:
+scp -r ./ root@edison.local:/node_app_slot/
