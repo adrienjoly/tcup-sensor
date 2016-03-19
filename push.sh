6fcb6e250837a20dd6ed44454809f6c733fd0438
Adrien Joly <adrien@whyd.com>
2016-03-19 15:12:16 +0100

    updated push script to use edison.local hostname instead of IP address. still not working

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 # rsync -av -e "ssh -A root@proxy ssh" ./ root@192.168.100.177/node_target
 # rsync -avz ./ root@192.168.100.177:/node_app_slot/
-rcp -r ./ root@192.168.100.177:/node_app_slot/
+# rcp -r ./ root@192.168.100.177:/node_app_slot/
+rcp -r ./ root@edison.local:/node_app_slot/
